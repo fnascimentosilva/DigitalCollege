1f529480fe8c9d002c89e169d861d7960888992f
fnascimentosilva <fabricionsilva26@gmail.com>
2025-01-13 19:50:06 -0400

    declarando variaveis

---
+++ Javascript/AULA12/script.js
@@ -0,0 +1,11 @@
+console.log("Olá mundo!");
+
+const pessoa = {
+    nome: "Fabricio",
+    idade: 37,
+    email: "fabricionsilva26@gmail.com",
+    dataNascimento: "06/04/1987"
+
+}
+
+console.log(pessoa);
