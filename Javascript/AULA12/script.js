27fa742ae7b4814c1e3ef0e77c01d82527e042af
fnascimentosilva <fabricionsilva26@gmail.com>
2025-01-13 20:14:33 -0400

    adicionando o pompt

--- Javascript/AULA12/script.js
+++ Javascript/AULA12/script.js
@@ -1,11 +1,16 @@
 console.log("Olá mundo!");
 
-const pessoa = {
+/* const pessoa = {
     nome: "Fabricio",
     idade: 37,
     email: "fabricionsilva26@gmail.com",
     dataNascimento: "06/04/1987"
 
-}
+} */
 
-console.log(pessoa);
+let nome = prompt("Insira seu nome: ")
+let idade = prompt("Insira seu idade: ")
+let email = prompt("Insira seu email: ")
+
+
+console.log(`O nome da pessoa é ${nome}, possui ${idade} anos e seu email é ${email} !`);
