004faf0364d158d27b4fbbd2c2e63f517b2d5bc7
fnascimentosilva <fabricionsilva26@gmail.com>
2025-01-15 18:37:51 -0400

    area quadrado

--- Javascript/AULA13/atividadeAula13.js
+++ Javascript/AULA13/atividadeAula13.js
@@ -1,8 +1,18 @@
-let nota1 = 8;
+/* calcule a média */
+/* let nota1 = 8;
 let nota2 = 5;
 let nota3 = 9;
 
 let media = (nota1+nota2+nota3)/3;
 
-alert(media.toFixed(2));
+console.log(media.toFixed(2)); */
+
+
+/* calcule a área de um quadrado */
+
+let ladoQuadrado = prompt("Insira o tamanho do lado do quadrado: ")
+
+let areaQuadrado = Math.pow(ladoQuadrado,2)
+
+alert(Number(areaQuadrado))
 
