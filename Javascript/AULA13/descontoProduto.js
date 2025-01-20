22f8161f408fc9bfd008e1b06e76ad42185bf017
fnascimentosilva <fabricionsilva26@gmail.com>
2025-01-20 13:09:26 -0400

    atividade desconto produto

---
+++ Javascript/AULA13/descontoProduto.js
@@ -0,0 +1,22 @@
+preco = 150.00;
+corEtiqueta = "VERmelho".toLowerCase();
+
+switch (corEtiqueta) {
+    case "verde":
+        console.log(`Seu desconto é de ${(preco * 0.1).toFixed(2)} R$`);
+        break;
+
+    case "amarelo":
+        console.log(`Seu desconto é de ${(preco * 0.2).toFixed(2)} R$`);
+        break;
+
+    case "azul":
+        console.log(`Seu desconto é de ${(preco * 0.3).toFixed(2)} R$`);
+        break;
+
+    case "vermelho":
+        console.log(`Seu desconto é de ${(preco * 0.4).toFixed(2)} R$`);
+        break;
+
+
+}
