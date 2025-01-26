c89e5591b42424901c2ff5cfa28e1758339228ef
fnascimentosilva <fabricionsilva26@gmail.com>
2025-01-25 23:19:08 -0400

    atividade multiplos de 3

---
+++ Javascript/ATIVIDADES_CONDICIONAIS/multiplos3.js
@@ -0,0 +1,9 @@
+/* Faça um programa que determine o mostre os cinco primeiros múltiplos de 3,
+considerando números maiores que 0. */
+
+for(let i = 0; i <= 5; i++){
+    if(i != 0){
+        console.log(`${i*3}`);
+        
+    }
+}
