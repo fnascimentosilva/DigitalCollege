56ab5028a33f124651ef8ff35106c05bac57932e
fnascimentosilva <fabricionsilva26@gmail.com>
2025-01-25 23:18:53 -0400

    atividade contagem regressiva

---
+++ Javascript/ATIVIDADES_CONDICIONAIS/contagemRegressivaWhile.js
@@ -0,0 +1,10 @@
+/* Faça um programa utilizando o comando while que mostra uma contagem regressiva na
+tela, iniciando em 10 e terminando em 0. Mostrar uma mensagem “FIM! ”após a contagem. */
+
+let numero = 10
+
+while(numero >= 0){
+    console.log(numero);
+    numero--;
+    
+}
