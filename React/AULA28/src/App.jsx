dfe4d0ebe30c40a2b57f4ea53844ff5e5c844935
fnascimentosilva <fabricionsilva26@gmail.com>
2025-03-23 22:01:57 -0400

    inserindo no App,jsx

--- React/AULA28/src/App.jsx
+++ React/AULA28/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import UserInfo from './components/UserInfo'
 import ConversorMoeda from './components/ConversorMoeda'
+import Calculadora from './components/Calculadora'
 
 function App() {
 
 
   return (
     <>
-      <UserInfo></UserInfo>
-      <ConversorMoeda></ConversorMoeda>
+      <Calculadora></Calculadora>
     </>
   )
 }
