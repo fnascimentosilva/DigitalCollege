ae2c0df7119ca156e8017551705d03c63c137ce3
fnascimentosilva <fabricionsilva26@gmail.com>
2025-03-31 19:47:50 -0400

    aplicacão bootstrap em react

---
+++ React/AULA31/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Carrossel from './components/Carrossel'
+
+function App() {
+  
+
+  return (
+    <>
+      <Carrossel></Carrossel>
+    </>
+  )
+}
+
+export default App
---
+++ React/AULA31/src/components/Carrossel.jsx
@@ -0,0 +1,61 @@
+// faça a instalação do bootstrap e a instalação do react-bootstrap
+// npm install react-bootstrap bootstrap
+
+// link o cdn do bootstrap no index.html
+/*<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+  integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+  crossorigin="anonymous"
+/>*/
+
+// crie um componente chamado carrosel que vai renderizar 3 imagens diferentes
+/*https://react-bootstrap.netlify.app/docs/components/carousel*/
+
+// utilize o componente carrosel no App.jsx
+
+import Carousel from 'react-bootstrap/Carousel';
+
+function Carrossel() {
+  return (
+    <Carousel data-bs-theme="dark">
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=First slide&bg=f5f5f5"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h5>First slide label</h5>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Second slide&bg=eee"
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+          <h5>Second slide label</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Third slide&bg=e5e5e5"
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h5>Third slide label</h5>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default Carrossel;
