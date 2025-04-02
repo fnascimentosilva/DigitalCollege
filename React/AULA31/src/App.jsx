8be86e8dfa35f1a1c395f24b4b96b86586af8cdf
fnascimentosilva <fabricionsilva26@gmail.com>
2025-04-01 23:39:20 -0400

    iniciando o card de produtos

--- React/AULA31/src/App.jsx
+++ React/AULA31/src/App.jsx
@@ -1,13 +1,23 @@
 
 import './App.css'
-import Carrossel from './components/Carrossel'
+import CardProduto from './components/CardProduto'
+
 
 function App() {
-  
+  let produtosTeste = [
+    {id: 1, nome: 'Produto 1', preco: 100, img: 'https://placehold.co/600x400', descricao: 'Produto 1 descrição' },
+    {id: 2, nome: 'Produto 2', preco: 200, img: 'https://placehold.co/600x400', descricao: 'Produto 2 descrição' },
+    {id: 3, nome: 'Produto 3', preco: 300, img: 'https://placehold.co/600x400', descricao: 'Produto 3 descrição' },
+    {id: 4, nome: 'Produto 4', preco: 400, img: 'https://placehold.co/600x400', descricao: 'Produto 4 descrição' },
+    {id: 5, nome: 'Produto 5', preco: 500, img: 'https://placehold.co/600x400', descricao: 'Produto 5 descrição' },
+    {id: 6, nome: 'Produto 6', preco: 600, img: 'https://placehold.co/600x400', descricao: 'Produto 6 descrição' }
+]
 
   return (
     <>
-      <Carrossel></Carrossel>
+       {produtosTeste.map((produto) => (
+        <CardProduto key={produto.id} produto={produto} />
+      ))}
     </>
   )
 }
